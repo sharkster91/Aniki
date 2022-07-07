13baa6e8ffdb7c5a2b4c9447e582eda3f270e49a
sharkster91 <79799951+sharkster91@users.noreply.github.com>
2022-07-06 23:09:41 -0400

    Initial commit

---
+++ Contents/mods/Aniki/media/lua/server/GachiSpawnList.lua
@@ -0,0 +1,12 @@
+if VehicleZoneDistribution then
+
+    VehicleZoneDistribution.parkingstall.vehicles["Base.Aniki1"] = {index = -1, spawnChance = 50};
+    VehicleZoneDistribution.parkingstall.vehicles["Base.Aniki2"] = {index = -1, spawnChance = 50};
+    
+    VehicleZoneDistribution.medium.vehicles["Base.Aniki1"] = {index = -1, spawnChance = 50};
+    VehicleZoneDistribution.medium.vehicles["Base.Aniki2"] = {index = -1, spawnChance = 50};
+    
+    VehicleZoneDistribution.good.vehicles["Base.Aniki"] = {index = -1, spawnChance = 50};
+    VehicleZoneDistribution.good.vehicles["Base.Aniki"] = {index = -1, spawnChance = 50};
+
+end
